91eb8faf505883db82d522690f633ffd6e9a5661
Ayush Yadav <149005942+Ayush-7355@users.noreply.github.com>
2025-08-19 23:45:02 +0530

    Time: 1 ms (57.63%), Space: 41.7 MB (78.91%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int cnt=0;
+        for(int i=0;i<stones.length();i++){
+            char ch=stones.charAt(i);
+            for(int j=0;j<jewels.length();j++){
+                if(ch==jewels.charAt(j)){
+                    cnt=cnt+1;
+                }
+            }
+        }
+        return cnt;
+    }
+}
